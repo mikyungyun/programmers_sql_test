2eb4b36be258cb8ca0ee73a378ca809ab6d77912
mikyungyun <yun.workk@gmail.com>
2020-11-04 20:15:12 +0900

    상위 n개 레코드

---
+++ 상위 n개 레코드.sql
@@ -0,0 +1,21 @@
+-- ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다.
+-- ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
+
+-- 동물 보호소에 가장 먼저 들어온 동물의 이름을 조회하는 SQL 문을 작성해주세요.
+
+SELECT NAME
+FROM 
+(SELECT * 
+FROM ANIMAL_INS
+ORDER BY DATETIME)
+WHERE ROWNUM = '1';
+
+
+-------------------다른 풀이------------------
+
+SELECT NAME 
+FROM 
+(SELECT * 
+FROM ANIMAL_INS 
+ORDER BY DATETIME) 
+WHERE ROWNUM = '1';
